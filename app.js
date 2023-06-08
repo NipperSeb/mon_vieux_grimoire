21e655e411d44e522b81bccd7b6966db1e1f2616
NipperSeb <sebastien.pincepoche@wanadoo.fr>
2023-06-08 15:14:29 +0200

    fit signup

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
 const express = require("express");
+const User = require("./models/users");
+const userRoutes = require("./routes/user");
+var cors = require("cors");
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.use((req, res, next) => {
-  console.log("Requête reçue !");
-  next();
+//Connect mongoose
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://sebpincemoi:sebastien@grimoire.rsa6kju.mongodb.net/test?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+//login
+app.post("/api/auth/signup", (req, res, next) => {
+  const user = new User({
+    email: req.body.email,
+    password: req.body.password,
+  });
+  user
+    .save()
+    .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+    .catch((error) => res.status(400).json({ error }));
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: "Votre requête a bien été reçue !" });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log("Réponse envoyée avec succès !");
-});
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
---
+++ controllers/users.js
@@ -0,0 +1,6 @@
+const bcrypt = require("bcrypt");
+const User = require("../models/users");
+
+exports.signup = (req, res, next) => {};
+
+exports.login = (req, res, next) => {};
